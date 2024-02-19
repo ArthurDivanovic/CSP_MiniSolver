ad9f699dd38ffa9f856f76e1ed3cf41b1f19f8f1
navaxel <axel.navarro@polytechnique.edu>
2024-02-19 11:20:46 +0100

    new strategies

--- src/strategies/variableselection.jl
+++ src/strategies/variableselection.jl
@@ -13,3 +13,37 @@ function(::MinDomainVariableSelection)(model::Model)
     end
     return selectedVariable
 end
+
+struct MostCenteredVariableSelection <: AbstractVariableSelection end
+
+function (::MostCenteredVariableSelection)(model::Model)
+    selectedId = nothing
+    n = length(model.variables)
+
+    bestScore = typemax(Int)
+
+    for (id,variable) in model.variables
+        if !isAssigned(variable) 
+            idCol = parse(Int, match(r"[0-9]+", id).match)
+            if abs(n/2 - idCol) < bestScore
+                bestScore = abs(n/2 - idCol)
+                selectedId = id
+            end
+        end 
+    end
+    return model.variables[selectedId]
+end
+
+struct RandomVariableSelection <: AbstractVariableSelection end
+
+function (::RandomVariableSelection)(model::Model)
+    notAssignedVariables = String[]
+    for (id,variable) in model.variables
+        if !isAssigned(variable) 
+            push!(notAssignedVariables, id)
+        end 
+    end
+
+    selectedId = notAssignedVariables[rand(1:length(notAssignedVariables))]
+    return model.variables[selectedId]
+end
